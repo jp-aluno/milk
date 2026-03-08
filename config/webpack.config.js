5440ff1f1b8a835e98a84056cc15b43e51d0a380
jp-aluno <43191369+jp-aluno@users.noreply.github.com>
2026-03-08 18:28:51 -0300

    Add files via upload

---
+++ config/webpack.config.js
@@ -0,0 +1,60 @@
+/*global __dirname, require, module*/
+const path = require('path');
+
+const root = path.join(__dirname, '..');
+const nodeRoot = path.join(__dirname, '..', 'node_modules');
+const outputPath = path.join(__dirname, '..', 'dist');
+
+const config = {
+  entry: {
+    base: root + '/base.js',
+    extra: root + '/extra.js',
+    image: root + '/image.js',
+    md1: root + '/md1.js',
+    minimal: root + '/minimal.js',
+    nonMinimal: root + '/nonMinimal.js',
+    all: root + '/all.js',
+    presetPackMeta: root + '/presetPackMeta.js',
+    imageData: root + '/imageData.js',
+  },
+  output: {
+    path: outputPath,
+    filename: '[name]',
+    library: '[name]ButterchurnPresets',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader?cacheDirectory',
+          options: {
+            plugins: ['@babel/plugin-transform-runtime'],
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  },
+  resolve: {
+    modules: [root, nodeRoot],
+    extensions: ['.js']
+  }
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+
+  if (argv.mode === 'production') {
+    config.output.filename += '.min';
+  }
+
+  config.output.filename += '.js';
+
+  return config;
+};
